d98b3f929c938ddea23fbaa686bb649267a917a0
Rezowan Talukder <rtalukder@contract.planetdds.com>
2022-05-14 16:23:20 +0600

    removed netlift adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,8 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
-	kit: {
-		target: '#svelte',
-		adapter: adapter()
-	}
 };
 
 export default config;
